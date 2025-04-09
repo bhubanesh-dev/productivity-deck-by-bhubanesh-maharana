0d4323e4241ffcba39c4695559ae9bc8f24a3741
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-04-09 19:53:24 +0530

    Attached useCountdownTimer hook to manage the pause, play and rest feature with audio

--- src/components/pomodoro/Container.jsx
+++ src/components/pomodoro/Container.jsx
@@ -1,38 +1,61 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import { Button } from "neetoui";
 import { FaPlay, FaPause, FaStop } from "react-icons/fa";
 
 import { TIMER } from "./constants";
+import { useCountdownTimer } from "./hooks/useCountdownTimer";
 import TimerType from "./TimerType";
 
+import pomodoroAlarm from "../../assets/pomodoroAlarm.mp3";
+
 const Container = () => {
-  const [selectedTimer, setSelectedTimer] = useState(TIMER[0]);
-  const [isTimerStart, setIsTimerStart] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const selectedTimer = TIMER[selectedIndex];
+
+  const handleTimerComplete = () => {
+    const nextIndex = (selectedIndex + 1) % TIMER.length;
+    setSelectedIndex(nextIndex);
+  };
+
+  const audioReference = useRef(null);
+
+  const { formattedTime, isRunning, start, pause } = useCountdownTimer(
+    selectedTimer.time,
+    handleTimerComplete,
+    audioReference
+  );
 
   return (
     <div className="flex h-full items-center justify-center">
-      <section className="rounded-lg border-2 border-gray-400  p-6">
-        <TimerType {...{ selectedTimer, setSelectedTimer }} />
+      <section className="rounded-lg border-2 border-gray-400 p-6">
+        <TimerType
+          selectedTimer={selectedTimer}
+          setSelectedTimer={timer => {
+            const index = TIMER.findIndex(t => t.type === timer.type);
+            setSelectedIndex(index);
+          }}
+        />
         <div className="mb-4 text-center text-7xl font-bold text-gray-800">
-          {selectedTimer.time}
+          {formattedTime}
         </div>
         <div className="flex justify-center gap-6">
           <Button
-            className="w-auto rounded px-4 py-2 "
-            icon={isTimerStart ? FaPause : FaPlay}
+            className="w-auto rounded px-4 py-2"
+            icon={isRunning ? FaPause : FaPlay}
             style="text"
-            onClick={() => setIsTimerStart(!isTimerStart)}
+            onClick={isRunning ? pause : start}
           />
-          {isTimerStart && (
+          {isRunning && (
             <Button
               icon={FaStop}
               style="text"
-              onClick={() => setIsTimerStart(false)}
+              onClick={() => handleTimerComplete()}
             />
           )}
         </div>
       </section>
+      <audio preload ref={audioReference} src={pomodoroAlarm} />
     </div>
   );
 };

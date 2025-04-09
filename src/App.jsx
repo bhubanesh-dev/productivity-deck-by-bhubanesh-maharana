9de7219dc79e1d1de1cce238359f30ed26a75b40
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-04-09 10:49:38 +0530

    Setup neetoUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
 import "./App.css";
-// eslint-disable-next-line import/extensions
-import logo from "./logo.svg";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img alt="logo" className="App-logo" src={logo} />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App = () => <div className="App">Productivity Deck</div>;
 
 export default App;

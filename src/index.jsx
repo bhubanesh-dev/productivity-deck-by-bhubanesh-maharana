b24dcdd828a829a227c888f8c401cefc0c89f88d
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-04-09 11:05:02 +0530

    Initialized i18n

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import "./common/i18n";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 

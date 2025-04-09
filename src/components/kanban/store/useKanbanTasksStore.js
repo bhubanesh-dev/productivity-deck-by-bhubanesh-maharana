a51c867571d200f9c026081eacd7e59e0bae0abf
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-04-09 15:58:52 +0530

    Clear the pre-defined store data

--- src/components/kanban/store/useKanbanTasksStore.js
+++ src/components/kanban/store/useKanbanTasksStore.js
@@ -17,19 +17,9 @@ const useKanbanTasksStore = create(
   persist(
     (set, get) => ({
       tasks: {
-        todo: [
-          { id: 1, taskName: "Item 1" },
-          { id: 2, taskName: "Item 2" },
-        ],
-        inProgress: [
-          { id: 3, taskName: "Item 3" },
-          { id: 4, taskName: "Item 4" },
-        ],
-        done: [
-          { id: 5, taskName: "Item 5" },
-          { id: 6, taskName: "Item 6" },
-          { id: 7, taskName: "Item 8" },
-        ],
+        todo: [],
+        inProgress: [],
+        done: [],
       },
       onDragEnd: result => {
         const { source, destination } = result;
